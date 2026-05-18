2afedec594d4e40a9c3b00eb44a130fe8c20d6f1
NaziaFarzana <231574097+contactnaziafarzana-code@users.noreply.github.com>
2026-05-18 14:37:53 +0600

    play.js

--- play.js
+++ play.js
@@ -1,1 +1,13 @@
 console.log("Basic Print method in JavaScript");
+let humanPlayer;
+let comPlayer;
+
+if (humanPlayer === "rock" && comPlayer === "scissors"){
+  if (humanPlayer === "paper" && comPlayer === "rock")
+  {
+    if(humanPlayer === "scissor" && comPlayer === "paper")
+      console.log("HumanPlayer win!!");
+  }
+}
+}
+else { console.log("comPlayer win!!"); }
