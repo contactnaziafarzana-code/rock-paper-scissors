654f8a0ec1fcd0b8f7147d9477a82e032a03334a
NaziaFarzana <231574097+contactnaziafarzana-code@users.noreply.github.com>
2026-05-18 16:31:56 +0600

    play.js

--- play.js
+++ play.js
@@ -1,9 +1,32 @@
-console.log("Basic logical operations & nested if else in JavaScript");
-let humanPlayer;
-let comPlayer;
 
-if (humanPlayer === "rock" && comPlayer === "scissors" || humanPlayer === "paper" && comPlayer === "rock" || humanPlayer === "scissor" && comPlayer === "paper"){
-console.log ("HumanPlayer win.");}
+let inputs= ['rock', 'paper', 'scissor'];
+let userInput = prompt("what is your choice? (rock,paper,scissors)");
+let humanPlayer = userInput;
+
+const randomItem = inputs[Math.floor(Math.random() * inputs.length)];
+
+let comPlayer=randomItem;
+
+
+
+
+
+
+
+
+
+
+
+if(!inputs.includes(humanPlayer))
+    alert("invalid");
+else if (humanPlayer === "rock" && comPlayer === "scissor" || humanPlayer === "paper" && comPlayer === "rock" || humanPlayer === "scissor" && comPlayer === "paper")
+{
+alert ("HumanPlayer win.");
+console.log("hum: ", humanPlayer , comPlayer)}
+   else if (humanPlayer === comPlayer){
+alert ("Tie!!");
+   console.log("hum: ", humanPlayer, comPlayer); }
   else {
-  console.log("comPlayer win.");
+  alert("comPlayer win.");
+  console.log("hum: ", humanPlayer, comPlayer);
 }
